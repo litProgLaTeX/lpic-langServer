a778bb7b0e9081fff9f010c020ab1b4e1f728cbd
Stephen Gaito <stephen@perceptisys.co.uk>
2023-04-17 11:52:16 +0100

    refactored lpic cli to use tmGrammars

--- lpicLangServer/lpicCli/cli.py
+++ lpicLangServer/lpicCli/cli.py
@@ -3,6 +3,7 @@ import argparse
 import pprint
 import yaml
 
+import tmGrammars.configuration
 from tmGrammars.grammar import Grammar
 from tmGrammars.documents import DocumentCache
 from tmGrammars.scopeActions import ScopeActions
@@ -13,72 +14,18 @@ def cli() :
 
   argParser  = argparse.ArgumentParser()
   argParser.add_argument('filePath', nargs='?',
-    help="The base ConTeXt file to load OR the tmLanguage syntax file to save")
-  argParser.add_argument('--save', metavar="base.scope", default='',
-      help="Save a full tmLanguage syntax for use with VScode")
+    help="The base ConTeXt file to load"
+  )
+  tmGrammars.configuration.addArgParseArguments(argParser)
 
-  argParser.add_argument('--check', action='store_true',
-    help="Check the loaded grammar for missing and extra patterns"
-  )
-  argParser.add_argument('--prune', action='store_true',
-    help="Prune unused patterns from grammar"
-  )
-  argParser.add_argument("--grammar", action='store_true',
-    help="Show the currently loaded Grammar"
-  )
-  argParser.add_argument("--patterns", action='store_true',
-    help="Show the patterns"
-  )
-  argParser.add_argument('--scopePaths', action='store_true',
-    help="List the scopePaths found in the current grammar"
-  )
-  argParser.add_argument("--rules", action='store_true',
-    help="Show the rules"
-  )
-  argParser.add_argument("--actions", action='store_true',
-    help="Show the actions"
-  )
   cliArgs = vars(argParser.parse_args())
-  #print(yaml.dump(cliArgs))
-  filePath = cliArgs['filePath']
 
-  
+  tmGrammars.configuration.loadConfig(cliArgs)
 
-  if cliArgs['actions'] :
-    ScopeActions.printActions()
-    return
-
-  Grammar.loadFromResourceDir('lpicSyntaxes')
   Grammar.collectRules()
   if cliArgs['prune'] : Grammar.pruneRules()
 
-  if cliArgs['patterns'] :
-    Grammar.printPatternReferences()
-    return
-
-  if cliArgs['rules'] :
-    Grammar.printRules()
-    return
-
-  if cliArgs['grammar'] :
-    Grammar.printGrammar()
-    return
-
-  if cliArgs['save'] : 
-    if Grammar.savedToFile(cliArgs['save'], filePath) :
-      print(f"Saved current syntax to the tmLanguage.json file:\n  {filePath}\n")
-    else :
-      print(f"base.scope [{cliArgs['save']}] not found in grammar\n")
-    return
-  
-  if cliArgs['check'] :
-    Grammar.printCheckRepositoryReport()
-    return
-
-  if cliArgs['scopePaths'] :
-    Grammar.printScopePaths()
-    return
-
+  filePath = cliArgs['filePath']
   if filePath == None :
     print("You MUST specify a filePath when extracting LPiC code")
     return
