001c991946b18f4fd8a07d7f27d545c5884fd19c
Stephen Gaito <stephen@perceptisys.co.uk>
2023-04-26 17:26:45 +0100

    begun port to javascript

---
+++ src/actions/lpicActions.mjs
@@ -0,0 +1,86 @@
+
+function loadedStrParam(disp, ctx, aMessage, callArgs) {
+  console.log("----------------------------------------------------------")
+  console.log("loadedStrParam")
+  console.log(aMessage)
+  console.log("----------------------------------------------------------")
+  ctx.append({
+    'method'   : 'loadedStrParam',
+    'msg'      : aMessage,
+    'kwargs'   : callArgs,
+    'dispType' : 'silly'
+  })
+}
+
+export function registerActions(ScopeActions) {
+
+  ScopeActions.addScopedAction(
+    'source.ansic.code',
+    import.meta.url,
+    { },
+    loadedStrParam
+  )
+
+  ScopeActions.addScopedAction(
+    'source.ansic.header',
+    import.meta.url,
+    { },
+    loadedStrParam
+  )
+}
+
+/*
+
+\definetyping%
+  [MkIVCode]%
+  [ option=TEX, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\definetyping%
+  [MpIVCode]%
+  [ option=MP, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\definetyping%
+  [LuaCode]%
+  [option=LUA, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\definetyping%
+  [LuaTemplate]%
+  [option=LUA, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\def\defaultCHeaderLicense{\dosingleempty\lpicSingleArgRelax}
+\def\createsCHeader{\dodoubleempty\lpicDoubleArgRelax}
+\def\addCHeaderLicense{\dodoubleempty\lpicDoubleArgRelax}
+
+\definetyping%
+  [CHeader]%
+  [option=CPP, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\def\defaultCCodeLicense{\dosingleempty\lpicSingleArgRelax}
+\def\buildsCCodeLibrary{\dodoubleempty\lpicDoubleArgRelax}
+\def\buildsCCodeApplication{\dodoubleempty\lpicDoubleArgRelax}
+\def\createsCCode{\dodoubleempty\lpicDoubleArgRelax}
+\\def\\addCCodeLicense{\dodoubleempty\lpicDoubleArgRelax}
+
+\\definetyping%
+  [CCode]%
+  [option=CPP, numbering=line, tab=2,
+    before={\noindent\startLitProgFrame}, after=\stopLitProgFrame
+  ]
+
+\\unexpanded\def\indexCCode[#1]{
+  \directlua{
+    thirddata.litProg.addCCodeIndex('#1')
+  }
+}
+
+*/
